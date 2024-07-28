d2e6ef20fb9d4ac1def2a9e848dddb129a51d341
arieycliu <ariel.yc.liu@gmail.com>
2024-07-28 19:21:26 -0400

    Hero done

--- landing-page/frontend/src/LandingPage.jsx
+++ landing-page/frontend/src/LandingPage.jsx
@@ -86,7 +86,7 @@ export default function LandingPage() {
             <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
             <Hero />
             <Box sx={{ bgcolor: 'background.default' }}>
-                <LogoCollection />
+                {/* <LogoCollection /> */}
                 <Features />
                 <Divider />
                 <Testimonials />
--- landing-page/frontend/src/components/Hero.jsx
+++ landing-page/frontend/src/components/Hero.jsx
@@ -19,18 +19,34 @@ const StyledBox = styled('div')(({ theme }) => ({
     borderRadius: theme.shape.borderRadius,
     outline: '1px solid',
     boxShadow: '0 0 12px 8px hsla(220, 25%, 80%, 0.2)',
-    backgroundImage: `url(${'/static/images/templates/templates-images/hero-light.png'})`,
+    backgroundImage: `url(${'/src/assets/robot.png'})`,
     outlineColor: 'hsla(220, 25%, 80%, 0.5)',
-    backgroundSize: 'cover',
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
     [theme.breakpoints.up('sm')]: {
         marginTop: theme.spacing(10),
         height: 700,
     },
     ...theme.applyStyles('dark', {
         boxShadow: '0 0 24px 12px hsla(210, 100%, 25%, 0.2)',
-        backgroundImage: `url(${'/static/images/templates/templates-images/hero-dark.png'})`,
+        backgroundImage: `url(${'/src/assets/robot.png'})`,
         outlineColor: 'hsla(210, 100%, 80%, 0.1)',
     }),
+}));
+
+const ImageBox = styled('div')(({ theme }) => ({
+    alignSelf: 'center',
+    backgroundPosition: 'center',
+    width: '100%',
+    height: 400,
+    marginTop: theme.spacing(8),
+    backgroundImage: `url(${'/src/assets/robot.png'})`,
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+    [theme.breakpoints.up('sm')]: {
+        marginTop: theme.spacing(10),
+        height: 700,
+    }
 }));
 
 export default function Hero() {
@@ -51,27 +67,45 @@ export default function Hero() {
             <Container
                 sx={{
                     display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    pt: { xs: 14, sm: 20 },
-                    pb: { xs: 8, sm: 12 },
+                    flexDirection: {
+                        sm: 'column',
+                        md: 'row'
+                    },
+                    alignItems: {
+                        sm: 'center',
+                        md: 'left'
+                    },
+                    pt: { xs: 14, sm: 14, md: 0 },
+                    pb: { xs: 8, sm: 12, md: 0 },
                 }}
             >
                 <Stack
                     spacing={2}
                     useFlexGap
-                    sx={{ alignItems: 'center', width: { xs: '100%', sm: '70%' } }}
+                    sx={{ 
+                        alignItems: {
+                            sm: 'center',
+                            md: 'flex-start'
+                        },
+                        width: { xs: '100%', sm: '70%' } }}
                 >
                     <Typography
                         variant="h1"
                         sx={{
                             display: 'flex',
                             flexDirection: { xs: 'column', sm: 'row' },
-                            alignItems: 'center',
-                            fontSize: 'clamp(3rem, 10vw, 3.5rem)',
+                            alignItems: {
+                                sm: 'center',
+                                md: 'flex-start'
+                            },
+                            fontSize: {
+                                xs: 'clamp(2rem, 10vw, 2rem)',
+                                sm: 'clamp(3rem, 10vw, 2rem)',
+                                md: 'clamp(3rem, 10vw, 3.5rem)'
+                            },
                         }}
                     >
-                        Our&nbsp;latest&nbsp;
+                        Real&#8209;Time&nbsp;
                         <Typography
                             component="span"
                             variant="h1"
@@ -83,19 +117,52 @@ export default function Hero() {
                                 }),
                             })}
                         >
-                            products
+                            Security
+                        </Typography>
+                    </Typography>
+                    <Typography
+                        variant="h1"
+                        sx={{
+                            display: 'flex',
+                            flexDirection: { xs: 'column', sm: 'row' },
+                            alignItems: {
+                                sm: 'center',
+                                md: 'flex-start'
+                            },
+                            fontSize: {
+                                xs: 'clamp(2rem, 10vw, 2rem)',
+                                sm: 'clamp(3rem, 10vw, 2rem)',
+                                md: 'clamp(3rem, 10vw, 3.5rem)'
+                            },
+                        }}
+                    >
+                        for&nbsp;Real&#8209;World&nbsp;
+                        <Typography
+                            component="span"
+                            variant="h1"
+                            sx={(theme) => ({
+                                fontSize: 'inherit',
+                                color: 'primary.main',
+                                ...theme.applyStyles('dark', {
+                                    color: 'primary.light',
+                                }),
+                            })}
+                        >
+                            Safety
                         </Typography>
                     </Typography>
                     <Typography
                         sx={{
-                            textAlign: 'center',
+                            textAlign: {
+                                sm: 'center',
+                                md: 'left'
+                            },
                             color: 'text.secondary',
                             width: { sm: '100%', md: '80%' },
                         }}
                     >
-                        Explore our cutting-edge dashboard, delivering high-quality solutions
-                        tailored to your needs. Elevate your experience with top-tier features
-                        and services.
+                        WatchDog provides top-notch remote monitoring solutions for schools, hospitals, construction sites, and more.
+                        Our state-of-the-art technology ensures you have eyes everywhere, anytime, and with unprecedented ease and reliability.
                     </Typography>
                     <Stack
                         direction={{ xs: 'column', sm: 'row' }}
@@ -129,15 +196,15 @@ export default function Hero() {
                             Get a demo
                         </Button>
                     </Stack>
-                    <Typography variant="caption" sx={{ textAlign: 'center' }}>
-                        By clicking &quot;Start now&quot; you agree to our&nbsp;
+                    {/* <Typography variant="caption" sx={{ textAlign: 'center' }}>
+                        We will be in contact in 2-3&nbsp;
                         <Link href="#" color="primary">
-                            Terms & Conditions
+                            working days
                         </Link>
                         .
-                    </Typography>
+                    </Typography> */}
                 </Stack>
-                <StyledBox id="image" />
+                <ImageBox id="image" />
             </Container>
         </Box>
     );
